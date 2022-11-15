9c330c553aad9e4e6856c862b366a3cf1e885456
Daniel Oliveira <danielcoliv98@gmail.com>
2022-11-15 09:24:00 -0300

    Initialzing

---
+++ backend/src/app.ts
@@ -0,0 +1,11 @@
+import cors from "cors";
+import express from "express";
+import "express-async-errors";
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+
+
+export default app;
